5799304762d602ef540768db2f52a56c149cbb23
cnguyen0691 <chaunguyen0691@gmail.com>
2019-04-29 18:56:10 -0400

    initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ATM atm = new ATM();
+
+        atm.accountCheck();
+
+    }
+}
